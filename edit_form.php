fd2feb508f88a195edcdd2ee5034b6b2c858c4eb
j.s <jsaylor@brandeis.edu>
2018-10-25 13:49:13 -0400

    typo!

--- edit_form.php
+++ edit_form.php
@@ -7,7 +7,7 @@ class block_staffenroll_edit_form extends block_edit_form {
         // Section header title according to language file.
         $mform->addElement('header', 'config_header', get_string('blocksettings', 'block_staffenroll'));
 
-        $mform->addElement('text', 'config_title', get_string('blocktitle', 'block_simplehtml'));
+        $mform->addElement('text', 'config_title', get_string('blocktitle', 'block_staffenroll'));
         $mform->setDefault('config_title', 'default config title');
         $mform->setType('config_title', PARAM_TEXT);
 
