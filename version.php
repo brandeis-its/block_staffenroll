fd63efa4c2dd54fe3b9b78d98cfd18b3eb0e8611
j.s <jsaylor@brandeis.edu>
2018-10-26 11:15:02 -0400

    incremented version

--- version.php
+++ version.php
@@ -1,6 +1,6 @@
 <?php
-$plugin->component = 'block_staffenroll'; 
-$plugin->version = 2018102505;
+$plugin->component = 'block_staffenroll';
+$plugin->version = 2018102600;
 
 // moodle 2.0
 $plugin->requires = 2010112400;
