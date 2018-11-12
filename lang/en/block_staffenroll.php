b5651a946af402238d43da2d7163afdfb2429639
j.s <jsaylor@brandeis.edu>
2018-11-12 14:37:21 -0500

    php syntax fix

--- lang/en/block_staffenroll.php
+++ lang/en/block_staffenroll.php
@@ -1,13 +1,13 @@
 <?php
 $string['staffenroll'] = 'Staff Enroll';
 $string['pluginname'] = 'Staff enrollments';
-$string['enableroleslabel'] = 'Enabling Roles'
+$string['enableroleslabel'] = 'Enabling Roles';
 $string['enablerolesdesc'] = 'Roles that allow users to be assigned staff enrollments';
 $string['studentenableroleslabel'] = 'Student enable roles';
 $string['studentenablerolesdesc'] = 'Select role enabling students for temp enrolments';
 $string['staffenableroleslabel'] = 'Staff enable roles';
 $string['staffenablerolesdesc'] = 'Select role enabling staff for temp enrolments';
-$string['assignroleslabel'] = 'Assignment Roles'
+$string['assignroleslabel'] = 'Assignment Roles';
 $string['assignrolesdesc'] = 'Roles to assigned users';
 $string['instructorroleslabel'] = 'Instructor roles';
 $string['instructorrolesdesc'] = 'Check box[es] of instructor role[s]';
--- version.php
+++ version.php
@@ -1,6 +1,6 @@
 <?php
 $plugin->component = 'block_staffenroll';
-$plugin->version = 2018111200;
+$plugin->version = 2018111201;
 
 // moodle 2.0
 $plugin->requires = 2010112400;
