5ac370ef32166349302eb5260db770a731fdbaca
j.s <jsaylor@brandeis.edu>
2018-11-27 16:39:44 -0500

    for upgrade to work

--- version.php
+++ version.php
@@ -1,6 +1,7 @@
 <?php
 $plugin->component = 'block_staffenroll';
-$plugin->version = 2018112700;
+$plugin->version = 2018112701;
 
+// FIXME: this should be changed to 3.5
 // moodle 2.0
 $plugin->requires = 2010112400;
